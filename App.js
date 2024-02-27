42abab876669a77c4a511ebaca7c79c414af552d
yexiaosu <wang.lan313@sjtu.edu.cn>
2024-02-26 21:14:18 -0800

    update color style: update the color for bottom tab bar

--- App.js
+++ App.js
@@ -1,22 +1,65 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeStackScreen from './screens/HomeStackScreen';
-import SettingsStackScreen from './screens/SettingsStackScreen';
-import HistoryStackScreen from './screens/HistoryStackScreen';
-import MessageStackScreen from './screens/MessageStackScreen';
+import * as React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { AntDesign } from '@expo/vector-icons'
+import { Ionicons } from '@expo/vector-icons'
 
-const Tab = createBottomTabNavigator();
+import HomeStackScreen from './screens/HomeStackScreen'
+import SettingsStackScreen from './screens/SettingsStackScreen'
+import HistoryStackScreen from './screens/HistoryStackScreen'
+import MessageStackScreen from './screens/MessageStackScreen'
+import { PrimaryColor } from './Style'
 
-export default function App() {
+const Tab = createBottomTabNavigator()
+
+export default function App () {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{ headerShown: false }}>
-        <Tab.Screen name="Home" component={HomeStackScreen} />
-        <Tab.Screen name="History" component={HistoryStackScreen} />
-        <Tab.Screen name="Messages" component={MessageStackScreen} />
-        <Tab.Screen name="Settings" component={SettingsStackScreen} />
+      <Tab.Navigator
+        screenOptions={{ headerShown: false, tabBarActiveTintColor: PrimaryColor }}
+        initialRouteName='Home'
+      >
+        <Tab.Screen
+          name='Home'
+          component={HomeStackScreen}
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({ color, size }) => (
+              <AntDesign name='home' size={size} color={color} />
+            )
+          }}
+        />
+        <Tab.Screen
+          name='History'
+          component={HistoryStackScreen}
+          options={{
+            tabBarLabel: 'History',
+            tabBarIcon: ({ color, size }) => (
+              <AntDesign name='clockcircleo' size={size} color={color} />
+            )
+          }}
+        />
+        <Tab.Screen
+          name='Messages'
+          component={MessageStackScreen}
+          options={{
+            tabBarLabel: 'Messages',
+            tabBarIcon: ({ color, size }) => (
+              <AntDesign name='message1' size={size} color={color} />
+            )
+          }}
+        />
+        <Tab.Screen
+          name='Settings'
+          component={SettingsStackScreen}
+          options={{
+            tabBarLabel: 'Settings',
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name='settings-outline' size={size} color={color} />
+            )
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
-  );
+  )
 }
---
+++ Style.js
@@ -0,0 +1,3 @@
+// Constants for styles in case there are situations that need style instead of className
+
+export const PrimaryColor = '#ac1e52'
