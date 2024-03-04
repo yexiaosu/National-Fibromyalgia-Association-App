701b46c42588528e185bd0a8a692f88f0e0cf7e4
yexiaosu <wang.lan313@sjtu.edu.cn>
2024-03-04 15:53:09 -0500

    Add header component: make a header component used for each page

---
+++ components/Header.js
@@ -0,0 +1,21 @@
+import * as React from 'react'
+import { Text, TouchableOpacity, View } from 'react-native'
+import { AntDesign } from '@expo/vector-icons'
+
+import { TextColor } from '../Style'
+
+export default function Header ({ title, navigation }) {
+  return (
+    <View className='flex flex-row w-full items-center justify-between'>
+      {navigation ? (
+        <TouchableOpacity className='pl-4' onPress={() => navigation.goBack()}>
+          <AntDesign name='left' size={24} color={TextColor} />
+        </TouchableOpacity>
+      ) : (
+        // placeholder
+        <View></View>
+      )}
+      <Text className='text-text text-3xl mr-4'>{title ? title : ' '}</Text>
+    </View>
+  )
+}
