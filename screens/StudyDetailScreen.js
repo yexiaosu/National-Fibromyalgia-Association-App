72d6b818adc574068780fa621555cf478ce56ae6
yexiaosu <wang.lan313@sjtu.edu.cn>
2024-03-04 16:52:40 -0500

    update study detail layout: make the accordion layout in study detail page

--- screens/StudyDetailScreen.js
+++ screens/StudyDetailScreen.js
@@ -8,7 +8,10 @@ import {
   View,
   Button
 } from 'react-native'
+import { AntDesign } from '@expo/vector-icons'
+
 import AccordionItem from '../components/Accordion'
+import Header from '../components/Header'
 
 export default function StudyDetailsScreen ({ route, navigation }) {
   const { studyId } = route.params
@@ -40,39 +43,36 @@ export default function StudyDetailsScreen ({ route, navigation }) {
 
   return (
     <SafeAreaView className='flex-1 items-center justify-center'>
+      <Header navigation={navigation} title={'Study Details'} />
       <Text>Details Screen for study {studyId}</Text>
       {/* Test accordion */}
-      {/* <ScrollView
-          contentInsetAdjustmentBehavior='automatic'
-          className='flex-1 flex-col w-11/12'
-          contentContainerStyle={{
-            alignItems: 'center',
-          }}
-        >
-          <AccordionItem title='Native development'>
-            <Text >
-              React Native lets you create truly native apps and doesn't
-              compromise your users' experiences. It provides a core set of
-              platform agnostic native components{' '}
-            </Text>
-          </AccordionItem>
-          <AccordionItem title='Fast refresh'>
-            <Text >
-              See your changes as soon as you save. With the power of
-              JavaScript, React Native lets you iterate at lightning speed.
-            </Text>
-          </AccordionItem>
-          <AccordionItem title='Cross-platform'>
-            <Text >
-              React components wrap existing native code and interact with
-              native APIs via React’s declarative UI paradigm and JavaScript.
-              This enables native app development for whole new teams of
-              developers
-            </Text>
-            <View ></View>
-            <Button title='See more...' />
-          </AccordionItem>
-        </ScrollView> */}
+      <ScrollView
+        contentInsetAdjustmentBehavior='automatic'
+        className='flex-1 flex-col w-11/12'
+        contentContainerStyle={{
+          alignItems: 'center'
+        }}
+      >
+        <AccordionItem title='Who can participate?'>
+          <Text className='text-text text-sm font-light'>
+            {study.requirements}
+          </Text>
+        </AccordionItem>
+        <AccordionItem title='What is invovled?'>
+          <Text className='text-text text-sm font-light'>
+            {study.procedure}
+          </Text>
+        </AccordionItem>
+        <AccordionItem title='Compensation'>
+          <Text className='text-text text-sm font-light'>
+            {study.compensation ? study.compensation : 'None'}
+          </Text>
+        </AccordionItem>
+        <AccordionItem title='Additional Information'>
+          <Text className='text-text text-sm font-light'>
+          </Text>
+        </AccordionItem>
+      </ScrollView>
     </SafeAreaView>
   )
 }
