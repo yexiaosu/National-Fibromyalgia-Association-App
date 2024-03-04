c73af8912fdda0366c886d43fc99e72460d596d9
yexiaosu <wang.lan313@sjtu.edu.cn>
2024-03-04 16:51:55 -0500

    update: mock data

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -1,8 +1,8 @@
-import React, {useState} from 'react';
-import { Button, View, Text, Switch } from 'react-native';
-import { useColorScheme } from "nativewind";
+import React, { useState } from 'react'
+import { Button, View, Text, Switch } from 'react-native'
+import { useColorScheme } from 'nativewind'
 
-import StudyCard from '../components/StudyCard';
+import StudyCard from '../components/StudyCard'
 
 export default function DashboardScreen ({ navigation }) {
   // dark mode test
@@ -14,15 +14,30 @@ export default function DashboardScreen ({ navigation }) {
   // };
 
   // studies mock data
-  const studies = [{
-    id: 1,
-    title: 'NFA 2007- PATIENT SURVEY',
-    logoUri: 'https://www.fmaware.org/wp-content/uploads/2020/02/ACR_CMYK.png',
-    participants: 2596,
-    isActive: false,
-    isOngoing: true,
-    description: '(Presented at the American College of Rheumatology) An internet survey of 2,596 people with fibromyalgia'
-  }]
+  const studies = [
+    {
+      id: 1,
+      title: 'European League Against Rheumatism',
+      logoUri:
+        'https://www.fmaware.org/wp-content/uploads/2020/02/st-scholastic.jpg',
+      participants: 172,
+      isActive: true,
+      isOngoing: true,
+      description:
+        'The European League Against Rheumatism (EULAR) continues to support a multidisciplinary approach, combining non-medicinal and medicinal treatments, to manage fibromyalgia. Master of Health Informatics student, Linda M. Feshami, BS, RHIT, CRC, CHC, CDIP of the College of St. Scholastica, with the collaboration of the NFA, is conducting research to determine if there is a significant difference in pain levels when a non-medicinal treatment is combined with an existing medicinal treatment.'
+    },
+    {
+      id: 2,
+      title: 'NFA 2007- PATIENT SURVEY',
+      logoUri:
+        'https://www.fmaware.org/wp-content/uploads/2020/02/ACR_CMYK.png',
+      participants: 2596,
+      isActive: false,
+      isOngoing: false,
+      description:
+        '(Presented at the American College of Rheumatology) An internet survey of 2,596 people with fibromyalgia'
+    }
+  ]
 
   return (
     <View className='flex-1 items-center justify-center bg-background'>
@@ -34,7 +49,9 @@ export default function DashboardScreen ({ navigation }) {
         onValueChange={toggleSwitch}
         value={isEnabled}
       /> */}
-      {studies.map((study, idx) => <StudyCard key={idx} navigation={navigation} study={study}></StudyCard>)}
+      {studies.map((study, idx) => (
+        <StudyCard key={idx} navigation={navigation} study={study}></StudyCard>
+      ))}
     </View>
   )
 }
--- screens/StudyDetailScreen.js
+++ screens/StudyDetailScreen.js
@@ -13,6 +13,31 @@ import AccordionItem from '../components/Accordion'
 export default function StudyDetailsScreen ({ route, navigation }) {
   const { studyId } = route.params
 
+  // studies mock data
+  const study = {
+    id: 1,
+    title: 'European League Against Rheumatism',
+    logoUri:
+      'https://www.fmaware.org/wp-content/uploads/2020/02/st-scholastic.jpg',
+    researchers: 'Linda Feshami, Dr. Danika Brinda',
+    survey:
+      'https://csscholastica.co1.qualtrics.com/jfe/form/SV_3CWsCMWkjBUDz81',
+    description:
+      'The European League Against Rheumatism (EULAR) continues to support a multidisciplinary approach, combining non-medicinal and medicinal treatments, to manage fibromyalgia. Master of Health Informatics student, Linda M. Feshami, BS, RHIT, CRC, CHC, CDIP of the College of St. Scholastica, with the collaboration of the NFA, is conducting research to determine if there is a significant difference in pain levels when a non-medicinal treatment is combined with an existing medicinal treatment.',
+    requirements:
+      'Study participants must be at least 18 years of age, diagnosed with fibromyalgia syndrome (FMS) by a health care practitioner, have experienced debilitating/life-altering pain symptoms due to FMS for a minimum of two years, and have added a non-medication treatment to an existing treatment based on medication alone.',
+    procedure:
+      'The study involves a short survey (approximately 10 min). Participants will be asked to rate pain levels at three separate points: 1) before treatment (either medicinal or non-medicinal), 2) during medicinal treatment only, and 3) medicinal combined with non-medicinal using a Likert Scale.',
+    compensation: '',
+    studyIdentifier: '',
+    relatedResearches: '',
+    videoUri: '',
+    expirationDate: '',
+    contact: 'Linda Feshami',
+    email: 'lfeshami@css.edu',
+    phone: '507-273-7764'
+  }
+
   return (
     <SafeAreaView className='flex-1 items-center justify-center'>
       <Text>Details Screen for study {studyId}</Text>
