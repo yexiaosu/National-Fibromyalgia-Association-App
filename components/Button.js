0f36aa75886db725ab509b0408325e272e6f7746
yexiaosu <wang.lan313@sjtu.edu.cn>
2024-04-02 19:42:17 -0400

    Settings: move the logout button to the end and change the style

--- components/Button.js
+++ components/Button.js
@@ -2,13 +2,13 @@ import * as React from 'react'
 import { Text, TouchableOpacity } from 'react-native'
 
 
-export default function Button ({ title, onPress }) {
+export default function Button ({ title, onPress, background='bg-primary', text='text-text-dark' }) {
   return (
     <TouchableOpacity
-      className='p-4 mt-4 bg-primary border-0 rounded-2xl items-center'
+      className={`p-4 mt-4 ${background} border-0 rounded-2xl items-center`}
       onPress={onPress}
     >
-      <Text className='text-text-dark text-base font-medium'>
+      <Text className={`${text} text-base font-medium`}>
         {title}
       </Text>
     </TouchableOpacity>
--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,18 +1,11 @@
 import React, { useEffect } from 'react'
-import {
-  View,
-  Text,
-  ScrollView,
-  SafeAreaView
-} from 'react-native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { View, Text, ScrollView, SafeAreaView } from 'react-native'
 import { useSelector, useDispatch } from 'react-redux'
 import { Ionicons } from '@expo/vector-icons'
 import { Avatar } from '@rneui/themed'
 
 import Header from '../components/Header'
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { signOut, getAuthUser } from '../AuthManager';
+import { signOut, getAuthUser } from '../AuthManager'
 import { loadProfile } from '../data/Actions'
 import { TextColor } from '../Style'
 import Button from '../components/Button'
@@ -33,107 +26,118 @@ export default function SettingsScreen ({ navigation }) {
         contentInsetAdjustmentBehavior='automatic'
         className='flex-1 flex-col w-11/12'
       >
-        <Button
-          className='w-1/5'
-          title="Logout"
-          onPress={async () => {
-            try {
-              await signOut();
-              navigation.navigate('Login')
-            } catch (error) {
-              Alert.alert("Sign In Error", error.message, [{ text: "OK" }])
-            }
-          }}
-        >
-        </Button>        <View className='items-center justify-center mt-8'>
-          <Avatar
-            size={128}
-            rounded
-            title={currentProfile.name[0]}
-            containerStyle={{ backgroundColor: '#3d4db7' }}
-          />
-          <Text className='text-text text-3xl font-medium mt-4'>
-            {currentProfile.name}
-          </Text>
-          <View>
-            <View className='flex flex-row w-full items-center mt-2'>
-              <Ionicons name='call-outline' size={22} color={TextColor} />
-              <Text className='text-text underline text-base font-light ml-2'>
-                {currentProfile.phoneNumber}
+        {currentProfile.uid && (
+          <>
+            <View className='items-center justify-center mt-8'>
+              <Avatar
+                size={128}
+                rounded
+                title={currentProfile.name[0]}
+                containerStyle={{ backgroundColor: '#3d4db7' }}
+              />
+              <Text className='text-text text-3xl font-medium mt-4'>
+                {currentProfile.name}
               </Text>
+              <View>
+                <View className='flex flex-row w-full items-center mt-2'>
+                  <Ionicons name='call-outline' size={22} color={TextColor} />
+                  <Text className='text-text underline text-base font-light ml-2'>
+                    {currentProfile.phoneNumber}
+                  </Text>
+                </View>
+                <View className='flex flex-row w-full items-center mt-2'>
+                  <Ionicons name='mail-outline' size={22} color={TextColor} />
+                  <Text className='text-text underline text-base font-light ml-2'>
+                    {currentProfile.email}
+                  </Text>
+                </View>
+              </View>
             </View>
-            <View className='flex flex-row w-full items-center mt-2'>
-              <Ionicons name='mail-outline' size={22} color={TextColor} />
-              <Text className='text-text underline text-base font-light ml-2'>
-                {currentProfile.email}
-              </Text>
+            <View className='w-11/12 mt-2'>
+              <Text className='text-text text-xl font-medium'>Information</Text>
+              <View className='flex flex-row w-full items-center justify-between mt-2'>
+                <Text className='text-text text-lg font-normal'>Gender</Text>
+                <Text className='text-text text-base font-light'>
+                  {currentProfile.gender}
+                </Text>
+              </View>
+              <View className='flex flex-row w-full items-center justify-between mt-2'>
+                <Text className='text-text text-lg font-normal'>Birthday</Text>
+                <Text className='text-text text-base font-light'>
+                  {currentProfile.birthday}
+                </Text>
+              </View>
+              <View className='flex flex-row w-full items-center justify-between mt-2'>
+                <Text className='text-text text-lg font-normal'>
+                  Diagnosed as fibromyalgia
+                </Text>
+                <Text className='text-text text-base font-light'>
+                  {currentProfile.isDiagnosed
+                    ? currentProfile.isDiagnosed
+                    : '-'}
+                </Text>
+              </View>
+              <View className='flex w-full items-start justify-center mt-2'>
+                <Text className='text-text text-lg font-normal'>
+                  Past phsical conditions
+                </Text>
+                <Text className='text-text text-base font-light'>
+                  {currentProfile.pastCondition
+                    ? currentProfile.pastCondition
+                    : '-'}
+                </Text>
+              </View>
+              <View className='flex w-full items-start justify-center mt-2'>
+                <Text className='text-text text-lg font-normal'>
+                  Current phsical conditions
+                </Text>
+                <Text className='text-text text-base font-light'>
+                  {currentProfile.curCondition
+                    ? currentProfile.curCondition
+                    : '-'}
+                </Text>
+              </View>
             </View>
-          </View>
-        </View>
-        <View className='w-11/12 mt-2'>
-          <Text className='text-text text-xl font-medium'>Information</Text>
-          <View className='flex flex-row w-full items-center justify-between mt-2'>
-            <Text className='text-text text-lg font-normal'>Gender</Text>
-            <Text className='text-text text-base font-light'>
-              {currentProfile.gender}
-            </Text>
-          </View>
-          <View className='flex flex-row w-full items-center justify-between mt-2'>
-            <Text className='text-text text-lg font-normal'>Birthday</Text>
-            <Text className='text-text text-base font-light'>
-              {currentProfile.birthday}
-            </Text>
-          </View>
-          <View className='flex flex-row w-full items-center justify-between mt-2'>
-            <Text className='text-text text-lg font-normal'>
-              Diagnosed as fibromyalgia
-            </Text>
-            <Text className='text-text text-base font-light'>
-              {currentProfile.isDiagnosed ? currentProfile.isDiagnosed : '-'}
-            </Text>
-          </View>
-          <View className='flex w-full items-start justify-center mt-2'>
-            <Text className='text-text text-lg font-normal'>
-              Past phsical conditions
-            </Text>
-            <Text className='text-text text-base font-light'>
-              {currentProfile.pastCondition
-                ? currentProfile.pastCondition
-                : '-'}
-            </Text>
-          </View>
-          <View className='flex w-full items-start justify-center mt-2'>
-            <Text className='text-text text-lg font-normal'>
-              Current phsical conditions
-            </Text>
-            <Text className='text-text text-base font-light'>
-              {currentProfile.curCondition ? currentProfile.curCondition : '-'}
-            </Text>
-          </View>
-        </View>
-        <View className='w-11/12 mt-2'>
-          <Text className='text-text text-xl font-medium'>Settings</Text>
-          <View className='flex w-full items-start justify-center mt-2'>
-            <Text className='text-text text-lg font-normal'>
-              Current phsical conditions
-            </Text>
-            <Text className='text-text text-base font-light'>
-              {currentProfile.visibility == 'AllStudyTeam'
-                ? 'All study teams'
-                : 'Only the study teams I show interest in'}
-            </Text>
-          </View>
-        </View>
-        <View className='pb-8'>
-          <Button
-            title='Edit profile & settings'
-            onPress={() =>
-              navigation.navigate('SettingEdit', {
-                uid: currentProfile.uid
-              })
-            }
-          />
-        </View>
+            <View className='w-11/12 mt-2'>
+              <Text className='text-text text-xl font-medium'>Settings</Text>
+              <View className='flex w-full items-start justify-center mt-2'>
+                <Text className='text-text text-lg font-normal'>
+                  Current phsical conditions
+                </Text>
+                <Text className='text-text text-base font-light'>
+                  {currentProfile.visibility == 'AllStudyTeam'
+                    ? 'All study teams'
+                    : 'Only the study teams I show interest in'}
+                </Text>
+              </View>
+            </View>
+            <View className='pb-8 w-3/4 self-center'>
+              <Button
+                title='Edit profile & settings'
+                onPress={() =>
+                  navigation.navigate('SettingEdit', {
+                    uid: currentProfile.uid
+                  })
+                }
+              />
+              <Button
+                title='Logout'
+                background='bg-border'
+                text='text-danger'
+                onPress={async () => {
+                  try {
+                    await signOut()
+                    navigation.navigate('Login')
+                  } catch (error) {
+                    Alert.alert('Sign In Error', error.message, [
+                      { text: 'OK' }
+                    ])
+                  }
+                }}
+              ></Button>
+            </View>
+          </>
+        )}
       </ScrollView>
     </SafeAreaView>
   )
